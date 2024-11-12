672680961acb88ea95167fea42f379ece7025311
Jasmit <138936262+Jasmit22@users.noreply.github.com>
2024-11-12 14:01:02 -0700

    front end structural changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-out": {
+          "0%, 100%": { opacity: 0 },
+          "10%, 90%": { opacity: 1 },
+        },
+      },
+      animation: {
+        "fade-in-out": "fade-in-out 3s ease-in-out",
+      },
+    },
   },
   plugins: [],
-}
-
+};
